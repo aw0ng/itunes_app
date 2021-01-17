92a9a4d229ea5b1e75f4aa634aa49f2156f32c0a
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-01-17 10:47:42 -0500

    seed database to favorite songs app

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+song = Song.create(title: "Humble", album: "DAMN.", artist: "Kendrick Lamar", year: "2017")
+song = Song.create(title: "Higher Love", album: "Higher Love", artist: "Whitney Houson & Kygo", year: 2019)
+song = Song.create(title: "You're All I need to Get By", album: "30 Greatest Hits", artist: "Aretha Franklin", year: 1985)
+song = Song.create(title: "How Sweet It Is (To Be Loved By You)", album: "Greatest Hits, Vol. 1", artist: "James Taylor", year: 1975)
+song = Song.create(title: "Young Forever", album: "Live In Brooklyn", artist: "Jay-Z feat. Beyonce", year: 2012)
